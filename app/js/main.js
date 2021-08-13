d5a1741a28b147081422900e2d915bb05fa169a4
mussonxp <mussonxp@gmail.com>
2021-08-13 22:40:09 +0300

    Britech. - base project

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.slider').slick({
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: true,
+    });
+
+})
